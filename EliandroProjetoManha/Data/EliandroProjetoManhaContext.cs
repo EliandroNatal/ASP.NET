e6b8ce73b587a0e2b66c3ccd25f762ab2de873c8
Eliandro Silva <eliandrosilva22@gmail.com>
2020-07-04 19:43:15 -0300

    Implementacao do MySql

---
+++ EliandroProjetoManha/Data/EliandroProjetoManhaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EliandroProjetoManha.Models;
+
+namespace EliandroProjetoManha.Data
+{
+    public class EliandroProjetoManhaContext : DbContext
+    {
+        public EliandroProjetoManhaContext (DbContextOptions<EliandroProjetoManhaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EliandroProjetoManha.Models.Departamento> Departamento { get; set; }
+    }
+}
--- EliandroProjetoManha/Startup.cs
+++ EliandroProjetoManha/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using EliandroProjetoManha.Data;
 
 namespace EliandroProjetoManha
 {
@@ -33,6 +35,10 @@ namespace EliandroProjetoManha
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<EliandroProjetoManhaContext>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("EliandroProjetoManhaContext"), builder => 
+                    builder.MigrationsAssembly("EliandroProjetoManha")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
